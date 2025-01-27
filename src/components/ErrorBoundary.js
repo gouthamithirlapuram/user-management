8e35410f5ed97342ad15c53453ae5ed9e6579ec4
gouthamithirlapuram <gouthamithirlapuram@gmail.com>
2025-01-27 17:22:08 +0530

    Update ErrorBoundary.js

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,1 +1,26 @@
+import React, { Component } from 'react';
 
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, errorMessage: '' };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true, errorMessage: error.message };
+  }
+
+  componentDidCatch(error, info) {
+    console.error('Error captured by ErrorBoundary: ', error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Error: {this.state.errorMessage}</h2>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
